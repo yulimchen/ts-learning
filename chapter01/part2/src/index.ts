51ea0aff119d3d7de6a857f99b3e8a9925042534
yulimchen <yulimchen@gmail.com>
2022-08-01 22:40:06 +0800

    feat: TS编译选项(4) 学习

--- chapter01/part2/src/index.ts
+++ chapter01/part2/src/index.ts
@@ -2,3 +2,11 @@ import { test } from './hello/app'
 const foobar = '123';
 // 这里是注释
 console.log(test);
+
+function fn(a: number, b: number) {
+  return a + b;
+}
+
+function fn2() {
+  console.log(this);
+}
