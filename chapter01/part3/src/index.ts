4c451eba7f4e72e621c18cb1255e39b6a8d577a3
yulimchen <yulimchen@gmail.com>
2022-09-02 00:24:29 +0800

    feat: webpack打包ts代码(3) 学习
    https://www.bilibili.com/video/BV1Xy4y1v7S2?p=12

--- chapter01/part3/src/index.ts
+++ chapter01/part3/src/index.ts
@@ -4,3 +4,7 @@ const foobar: string = 'foobar';
 console.log(foobar);
 console.log(helloworld);
 
+const obj = { name: 'foobar' }
+const tempObj = { ...obj }
+
+console.log(tempObj);
--- chapter01/part3/webpack.config.js
+++ chapter01/part3/webpack.config.js
@@ -11,7 +11,11 @@ module.exports = {
   // 输出
   output: {
     path: path.resolve(__dirname, "dist"),
-    filename: "bundle.js"
+    filename: "bundle.js",
+    // 不要使用箭头函数
+    environment: {
+      arrowFunction: false
+    }
   },
 
   // 模块
@@ -21,7 +25,35 @@ module.exports = {
       {
         // 指定规则生效文件
         test: /\.ts$/,
-        use: "ts-loader",
+        use: [
+          // 配置 babel
+          {
+            // 指定加载器
+            loader: "babel-loader",
+            // 配置 babel
+            options: {
+              // 设置预定义的环境
+              presets: [
+                [
+                  // 指定环境的插件
+                  "@babel/preset-env",
+                  // 配置信息
+                  {
+                    // 要兼容的目标浏览器
+                    targets: {
+                      "ie": "11"
+                    },
+                    // 指定 corejs 版本
+                    "corejs": "3",
+                    // 使用 corejs 的方式 "usage" 表示按需加载
+                    "useBuiltIns": "usage"
+                  }
+                ]
+              ]
+            }
+          },
+          "ts-loader"
+        ],
         exclude: path.resolve(__dirname, "node_modules")
       }
     ]
